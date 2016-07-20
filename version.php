3f204bdc1ce6edd03aab252548a4e93fd07fc14f
Simon Eisenmann <simon@struktur.de>
2016-07-20 13:38:41 +0200

    Imported Upstream version 9.0.53

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php 
-$OC_Version = array(9,0,52,0);
-$OC_VersionString = '9.0.52';
+$OC_Version = array(9,0,53,0);
+$OC_VersionString = '9.0.53';
 $OC_Edition = '';
 $OC_Channel = 'stable';
 $OC_VersionCanBeUpgradedFrom = array(8,2);
-$OC_Build = '2016-07-05T09:09:27+00:00 6d9809ef452d73b53d3d70493d9a92ee64bd528f';
+$OC_Build = '2016-07-18T14:33:51+00:00 6d9809ef452d73b53d3d70493d9a92ee64bd528f';
