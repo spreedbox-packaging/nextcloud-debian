ce112abd40da14dc62706ac4f8510417bcc1aa14
Simon Eisenmann <simon@struktur.de>
2016-07-27 18:01:21 +0200

    Update cron conditions

--- debian/nextcloud-server-php5.cron.d
+++ debian/nextcloud-server-php5.cron.d
@@ -1,3 +1,3 @@
 # Run Nextcloud cronjob every 15 minutes.
 
-*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php5 -f /usr/lib/nextcloud/www/cron.php; fi
+*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/nextcloud-server-php5" -a -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php5 -f /usr/lib/nextcloud/www/cron.php; fi
--- debian/nextcloud-server-php7.0.cron.d
+++ debian/nextcloud-server-php7.0.cron.d
@@ -1,3 +1,3 @@
 # Run Nextcloud cronjob every 15 minutes.
 
-*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php7.0 -f /usr/lib/nextcloud/www/cron.php; fi
+*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/nextcloud-server-php7.0" -a -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php7.0 -f /usr/lib/nextcloud/www/cron.php; fi
