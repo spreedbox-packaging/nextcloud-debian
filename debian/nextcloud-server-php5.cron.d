9d9b88d6c8b35ae207ec1235870eb27761b1e011
Simon Eisenmann <simon@struktur.de>
2016-07-27 16:32:52 +0200

    Improve PHP5/PHP7.0 compatibility

---
+++ debian/nextcloud-server-php5.cron.d
@@ -0,0 +1,3 @@
+# Run Nextcloud cronjob every 15 minutes.
+
+*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php5 -f /usr/lib/nextcloud/www/cron.php; fi
--- debian/nextcloud-server.cron.d
+++
@@ -1,3 +0,0 @@
-# run nextcloud cronjob every 15 minutes
-
-*/15 * * * *	www-data	if [ -e "/usr/lib/nextcloud/www/cron.php" ]; then /usr/bin/php -f /usr/lib/nextcloud/www/cron.php; fi
